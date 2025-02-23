6338d3689e5330a73914de65a77d43b7be387fbc
JiY <jiyong3954@gmail.com>
2025-02-23 22:31:14 +0900

    feat : `TILApi` 추가

---
+++ data-source/remote/server/src/main/java/com/dd2d/data_source/remote/server/til/dto/request/TILCreateRequestDto.kt
@@ -0,0 +1,9 @@
+package com.dd2d.data_source.remote.server.til.dto.request
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TILCreateRequestDto(
+    val title: String,
+    val content: String,
+)
