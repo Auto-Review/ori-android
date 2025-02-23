b8215964f79a206618641a92282aaf53f3b1cfe2
JiY <jiyong3954@gmail.com>
2025-02-24 00:53:06 +0900

    feat : 기본 컴포넌트 `SlotMainText` 추가

---
+++ core/presentation/src/main/java/com/dd2d/core/presentation/slot_main_text/SlotMainText.kt
@@ -0,0 +1,73 @@
+package com.dd2d.core.presentation.slot_main_text
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dd2d.core.presentation.main_text.MainText
+
+@Composable
+fun SlotMainText(
+    text: String,
+    modifier: Modifier = Modifier,
+    slotVerticalAlignment: Alignment.Vertical = Alignment.CenterVertically,
+    slotHorizontalArrangement: Arrangement.Horizontal = Arrangement.SpaceBetween,
+    fillTextWidth: Boolean = false,
+    prefix: @Composable (() -> Unit)? = null,
+    prefixEndPadding: Dp = 0.dp,
+    suffix: @Composable (() -> Unit)? = null,
+    suffixStartPadding: Dp = 0.dp,
+    color: Color = Color.Black,
+    fontSize: TextUnit = 14.sp,
+    fontWeight: FontWeight = FontWeight.Normal,
+    overFlow: TextOverflow = TextOverflow.Ellipsis,
+    textAlign: TextAlign = TextAlign.Start,
+    softWrap: Boolean = true,
+    maxLine: Int = 1,
+    minLine: Int = 1,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    fontFamily: FontFamily? = null,
+    textStyle: TextStyle = LocalTextStyle.current,
+) {
+    Row(
+        verticalAlignment = slotVerticalAlignment,
+        horizontalArrangement = slotHorizontalArrangement,
+        modifier = modifier
+    ) {
+        prefix?.invoke()
+        Spacer(Modifier.width(prefixEndPadding))
+        MainText(
+            text = text,
+            color = color,
+            fontSize = fontSize,
+            fontWeight = fontWeight,
+            overFlow = overFlow,
+            textAlign = textAlign,
+            softWrap = softWrap,
+            maxLine = maxLine,
+            minLine = minLine,
+            lineHeight = lineHeight,
+            letterSpacing = letterSpacing,
+            textStyle = textStyle,
+            fontFamily = fontFamily,
+            modifier = Modifier.weight(1F, fillTextWidth)
+        )
+        Spacer(Modifier.width(suffixStartPadding))
+        suffix?.invoke()
+    }
+}
