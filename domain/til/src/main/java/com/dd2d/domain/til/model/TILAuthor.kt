5a0949976374a81c7abb9447e0ee9acb382bb856
JiY <jiyong3954@gmail.com>
2025-02-24 00:17:21 +0900

    feat : `TIL` 더미 데이터 추가

--- domain/til/src/main/java/com/dd2d/domain/til/model/TILAuthor.kt
+++ domain/til/src/main/java/com/dd2d/domain/til/model/TILAuthor.kt
@@ -4,4 +4,12 @@ data class TILAuthor(
     val id: Int,
     val nickname: String,
     val email: String,
-)
+) {
+    companion object {
+        fun dummy(id: Int = 1) = TILAuthor(
+            id = id,
+            nickname = "닉네임",
+            email = "jiyong3954@gmail.com",
+        )
+    }
+}
