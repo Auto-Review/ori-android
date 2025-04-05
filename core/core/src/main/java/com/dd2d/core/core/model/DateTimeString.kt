bfa59f72c663e0dacb05b804c79bdf4d5183cfb2
JiY <jiyong3954@gmail.com>
2025-04-05 21:10:09 +0900

    feat : DateTimeString.kt 추가   * 날짜 시간 문자열 typealias

---
+++ core/core/src/main/java/com/dd2d/core/core/model/DateTimeString.kt
@@ -0,0 +1,10 @@
+package com.dd2d.core.core.model
+
+/** UTC 문자열. `yyyy-MM-DDTHH:mm:ss.SSSZ` */
+typealias UTCString = String
+
+/** 날짜 시간 문자열. `yyyy-MM-DD HH:mm:ss` */
+typealias DateTimeString = String
+
+/** 날짜 문자열. `yyyy-MM-DD` */
+typealias DateString = String
