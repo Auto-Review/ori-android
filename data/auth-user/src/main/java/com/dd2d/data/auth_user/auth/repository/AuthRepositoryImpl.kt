9bdd78d6af4c951ee1067a3b78826462d37ecb21
JiY <jiyong3954@gmail.com>
2025-05-01 19:45:37 +0900

    impl AuthRepositoryImpl.getAuthState()

--- data/auth-user/src/main/java/com/dd2d/data/auth_user/auth/repository/AuthRepositoryImpl.kt
+++ data/auth-user/src/main/java/com/dd2d/data/auth_user/auth/repository/AuthRepositoryImpl.kt
@@ -2,10 +2,13 @@ package com.dd2d.data.auth_user.auth.repository
 
 import com.dd2d.core.core.state.DataState
 import com.dd2d.core.core.state.asDataState
+import com.dd2d.core.data_store_manager.DataStoreManager
+import com.dd2d.core.data_store_manager.Keys
 import com.dd2d.data.auth_user.auth.mapper.toAuthRequestDto
 import com.dd2d.data_source.remote.server.auth.AuthApi
 import com.dd2d.domain.auth_user.auth.model.AuthRequester
 import com.dd2d.domain.auth_user.auth.model.AuthResult
+import com.dd2d.domain.auth_user.auth.model.AuthState
 import com.dd2d.domain.auth_user.auth.repository.AuthRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -13,6 +16,7 @@ import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val authApi: AuthApi,
+    private val dataStoreManager: DataStoreManager,
 ): AuthRepository {
     override fun auth(requester: AuthRequester): Flow<DataState<AuthResult>> = flow {
         val response = authApi.auth(requester.toAuthRequestDto())
@@ -24,4 +28,12 @@ class AuthRepositoryImpl @Inject constructor(
             )
         )
     }.asDataState()
+
+    override suspend fun getAuthState(): Result<AuthState> = dataStoreManager
+        .runCatching {
+            getValueByKey(key = Keys.getAuthStateKey(), default = AuthState.SignOut.ordinal)
+        }
+        .map { ordinal ->
+            AuthState.entries[ordinal]
+        }
 }
