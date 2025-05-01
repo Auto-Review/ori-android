633d7bdf0d5d114e4330e5556ab5cbc1a209fb0e
JiY <jiyong3954@gmail.com>
2025-05-01 19:31:25 +0900

    deprecate LocalSettingRepository functions - saveAuthToken - getSignInState - setSignInState

--- domain/auth-user/src/main/java/com/dd2d/domain/auth_user/auth/repository/AuthRepository.kt
+++ domain/auth-user/src/main/java/com/dd2d/domain/auth_user/auth/repository/AuthRepository.kt
@@ -3,8 +3,10 @@ package com.dd2d.domain.auth_user.auth.repository
 import com.dd2d.core.core.state.DataState
 import com.dd2d.domain.auth_user.auth.model.AuthRequester
 import com.dd2d.domain.auth_user.auth.model.AuthResult
+import com.dd2d.domain.auth_user.auth.model.AuthState
 import kotlinx.coroutines.flow.Flow
 
 interface AuthRepository {
     fun auth(requester: AuthRequester): Flow<DataState<AuthResult>>
+    suspend fun getAuthState(): Result<AuthState>
 }
--- domain/local-setting/src/main/java/com/dd2d/domain/local_setting/model/SignInState.kt
+++ domain/local-setting/src/main/java/com/dd2d/domain/local_setting/model/SignInState.kt
@@ -1,5 +1,6 @@
 package com.dd2d.domain.local_setting.model
 
+@Deprecated("Use [auth-user:SignInState] instead")
 enum class SignInState {
     SignOut,
     SignIn
