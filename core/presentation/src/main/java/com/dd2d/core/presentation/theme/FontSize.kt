aaa1cdf5a2879741dd5a2371931390fa5ccf15b0
JiY <jiyong3954@gmail.com>
2025-05-02 15:13:27 +0900

    add : tp - `FontScale`에 영향받지 않는 고정적인 `TextUnit`

---
+++ core/presentation/src/main/java/com/dd2d/core/presentation/theme/FontSize.kt
@@ -0,0 +1,15 @@
+package com.dd2d.core.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+
+val Int.tp: TextUnit
+    @Composable get() = with(LocalDensity.current) { this@tp.dp.toSp() }
+
+val Float.tp: TextUnit
+    @Composable get() = with(LocalDensity.current) { this@tp.dp.toSp() }
+
+val Double.tp: TextUnit
+    @Composable get() = with(LocalDensity.current) { this@tp.dp.toSp() }
