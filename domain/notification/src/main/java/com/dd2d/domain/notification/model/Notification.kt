2f96c2bd2d036460338e33e6d3f74fbec1db9aff
JiY <jiyong3954@gmail.com>
2025-05-03 13:38:44 +0900

    add :domain:notification 추가

---
+++ domain/notification/src/main/java/com/dd2d/domain/notification/model/Notification.kt
@@ -0,0 +1,27 @@
+package com.dd2d.domain.notification.model
+
+import com.dd2d.core.core.model.DateString
+import com.dd2d.core.core.util.format
+import java.time.LocalDate
+
+data class Notification(
+    val id: Int,
+    val content: String,
+    val noticeAt: DateString,
+    val checked: Boolean,
+    val state: State
+) {
+    enum class State {
+        PENDING, COMPLETE
+    }
+
+    companion object {
+        fun dummy(id: Int = 1) = Notification(
+            id = id,
+            content = "알림 내용 $id",
+            noticeAt = LocalDate.now().format("yyyy-MM-dd"),
+            checked = true,
+            state = State.entries.random(),
+        )
+    }
+}
---
+++ domain/notification/src/main/java/com/dd2d/domain/notification/repository/NotificationRepository.kt
@@ -0,0 +1,12 @@
+package com.dd2d.domain.notification.repository
+
+import com.dd2d.core.core.state.DataState
+import com.dd2d.domain.notification.model.Notification
+import kotlinx.coroutines.flow.Flow
+import java.time.YearMonth
+
+interface NotificationRepository {
+    fun getMyNotifications(): Flow<DataState<List<Notification>>>
+    fun getMyNotificationOnDate(yearMonth: YearMonth): Flow<DataState<List<Notification>>>
+    suspend fun toggleNotification(id: Int): Result<Unit>
+}
