8b3417e2bda0e59a9d6c7f3d2dd806127670e9fe
JiY <jiyong3954@gmail.com>
2025-05-24 19:19:55 +0900

    add : Stateful

---
+++ core/presentation/src/main/java/com/dd2d/core/presentation/state/Stateful.kt
@@ -0,0 +1,93 @@
+package com.dd2d.core.presentation.state
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+
+sealed interface Stateful<out T> {
+    data object Loading: Stateful<Nothing>
+    data class Error(val cause: Throwable): Stateful<Nothing>
+    data class Success<T>(val data: T): Stateful<T>
+}
+
+
+fun <T> statefulResult(block: suspend () -> Result<T>): Flow<Stateful<T>> {
+    return flow { emit(Stateful.Success(block().getOrThrow())) }
+        .onStart { Stateful.Loading }
+        .catch { Stateful.Error(it) }
+}
+
+fun <T, R> T.withStatefulResult(block: suspend T.() -> Result<R>): Flow<Stateful<R>> {
+    return flow { emit(Stateful.Success(this@withStatefulResult.block().getOrThrow())) }
+        .onStart { Stateful.Loading }
+        .catch { Stateful.Error(it) }
+}
+
+fun <T> Result<T>.asStatefulResult(): Flow<Stateful<T>> {
+    return flow { emit(Stateful.Success(this@asStatefulResult.getOrThrow())) }
+        .onStart { Stateful.Loading }
+        .catch { Stateful.Error(it) }
+}
+
+
+
+fun <T> Flow<Stateful<T>>.onLoading(block: suspend () -> Unit): Flow<Stateful<T>> {
+    return onEach { state ->
+        if(state is Stateful.Loading) { block() }
+    }
+}
+
+fun <T> Flow<Stateful<T>>.onError(block: suspend (cause: Throwable) -> Unit): Flow<Stateful<T>> {
+    return onEach { state ->
+        if(state is Stateful.Error) { block(state.cause) }
+    }
+}
+
+fun <T> Flow<Stateful<T>>.onSuccess(block: suspend (data: T) -> Unit): Flow<Stateful<T>> {
+    return onEach { state ->
+        if(state is Stateful.Success) { block(state.data) }
+    }
+}
+
+
+fun <T> Flow<Stateful<T>>.onEachState(
+    onLoading: suspend () -> Unit,
+    onError: suspend (cause: Throwable) -> Unit,
+    onSuccess: suspend (data: T) -> Unit,
+): Flow<Stateful<T>> {
+    return onEach { state ->
+        when(state) {
+            is Stateful.Loading -> onLoading()
+            is Stateful.Error -> onError(state.cause)
+            is Stateful.Success-> onSuccess(state.data)
+        }
+    }
+}
+
+
+fun <T, R> Flow<Stateful<T>>.mapSuccess(onSuccess: (T) -> R): Flow<Stateful<R>> {
+    return map { state ->
+        when(state) {
+            is Stateful.Loading -> Stateful.Loading
+            is Stateful.Error -> Stateful.Error(state.cause)
+            is Stateful.Success -> Stateful.Success( onSuccess(state.data))
+        }
+    }
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun <T, R> Flow<Stateful<T>>.flatMapSuccess(onSuccess: (T) -> Flow<Stateful<R>>): Flow<Stateful<R>> {
+    return flatMapLatest {
+        when(it) {
+            is Stateful.Loading -> flowOf(Stateful.Loading)
+            is Stateful.Error -> flowOf(Stateful.Error(it.cause))
+            is Stateful.Success -> onSuccess(it.data)
+        }
+    }
+}
