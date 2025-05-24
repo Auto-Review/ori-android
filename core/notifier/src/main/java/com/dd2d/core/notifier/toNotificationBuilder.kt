b3ed6ea102edd08bc1a559dec61c28ad1856366c
JiY <jiyong3954@gmail.com>
2025-05-24 18:55:53 +0900

    feat : `:core:notifier` 추가

---
+++ core/notifier/src/main/java/com/dd2d/core/notifier/toNotificationBuilder.kt
@@ -0,0 +1,35 @@
+package com.dd2d.core.notifier
+
+import android.content.Context
+import androidx.core.app.NotificationCompat
+
+internal fun NotifyScope.toNotificationBuilder(
+    context: Context,
+    channelId: String
+): NotificationCompat.Builder {
+    require(iconRes != -1) { "아이콘을 설정해주세요." }
+
+    return NotificationCompat.Builder(context, channelId)
+        .setContentTitle(title)
+        .setContentText(content)
+        .setSmallIcon(iconRes)
+        .setContentIntent(pendingIntent)
+        .setAutoCancel(autoCancel)
+        .setGroupSummary(groupSummary)
+        .setGroup(groupKey)
+        .setPriority(priority)
+        .setStyle(style)
+        .setVibrate(vibrationPattern?.toLongArray()?: longArrayOf())
+        .setSound(soundUri)
+        .setLargeIcon(largeIcon)
+        .setColor(color)
+        .setSilent(isSilent)
+        .setProgress(progressMax, progressCurrent, progressIndeterminate)
+        .setSubText(subText)
+        .setTicker(ticker)
+        .setOnlyAlertOnce(onlyAlertOnce)
+        .setOngoing(ongoing)
+        .also { builder ->
+            actions.forEach(builder::addAction)
+        }
+}
