a0aad0292e93716db1b8413379f0ea5310322f6c
JiY <jiyong3954@gmail.com>
2025-05-31 17:19:01 +0900

    remove : Paging3 add : RefreshLazyColumn(v2), LazyListController

--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -49,8 +49,5 @@ dependencies {
 
     implementation(libs.androidx.navigation.compose)
 
-    api(libs.androidx.paging.runtime.ktx)
-    api(libs.androidx.paging.compose)
-
     implementation(project(":core:core"))
 }
