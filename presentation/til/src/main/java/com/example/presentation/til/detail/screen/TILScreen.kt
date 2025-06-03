ad93e27795a25bde9ff0a77df869be85ebd3837c
JiY <jiyong3954@gmail.com>
2025-06-04 01:57:42 +0900

    update : TILScreen / stateful 적용

--- presentation/til/src/main/java/com/example/presentation/til/detail/screen/TILScreen.kt
+++ presentation/til/src/main/java/com/example/presentation/til/detail/screen/TILScreen.kt
@@ -9,10 +9,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.dd2d.core.core.state.DataState
 import com.dd2d.core.presentation.app_bar.PostTapBar
 import com.dd2d.core.presentation.dialog.ErrorDialog
 import com.dd2d.core.presentation.dialog.LoadingDialog
+import com.dd2d.core.presentation.state.Stateful
+import com.dd2d.domain.til.model.TIL
 import com.example.presentation.til.detail.content.TILScreenContent
 import com.example.presentation.til.detail.view_model.TILViewModel
 
@@ -22,16 +23,18 @@ fun TILScreen(
     modifier: Modifier = Modifier
 ) {
     val viewModel = hiltViewModel<TILViewModel>()
+    val userState by viewModel.userState.collectAsStateWithLifecycle()
     val tilState by viewModel.tilState.collectAsStateWithLifecycle()
+    val isAuthor by viewModel.isAuthor.collectAsStateWithLifecycle()
 
     Scaffold(
         topBar = {
             PostTapBar(
-                title = (tilState as? DataState.Success)?.data?.title ?: "",
+                title = (tilState as? Stateful.Success<TIL>)?.data?.title ?: "",
                 onBack = onBack,
                 isScrapped = viewModel.isScrapped,
                 toggleScrap = viewModel::scrap,
-                isAuthor = false,
+                isAuthor = isAuthor,
                 onUpdateClick = {},
                 onDeleteClick = {},
             )
@@ -39,16 +42,16 @@ fun TILScreen(
         modifier = modifier
     ) { inner ->
         when(tilState) {
-            is DataState.Loading -> LoadingDialog()
-            is DataState.Error -> {
+            is Stateful.Loading -> LoadingDialog()
+            is Stateful.Error -> {
                 ErrorDialog(
-                    exception = (tilState as DataState.Error).exception,
+                    throwable = (tilState as Stateful.Error).cause,
                     onConfirm = onBack
                 )
             }
-            is DataState.Success -> {
+            is Stateful.Success -> {
                 TILScreenContent(
-                    til = (tilState as DataState.Success).data,
+                    til = (tilState as Stateful.Success).data,
                     modifier = Modifier
                         .consumeWindowInsets(inner)
                         .fillMaxSize()
